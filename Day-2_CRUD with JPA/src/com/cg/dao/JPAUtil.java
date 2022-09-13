ed83c6692672f07673ae0c1295a874307515939c
KRUTIKA <KRUTIKA@LAPTOP-C7BBI500>
2022-09-13 14:31:32 +0530

    commit

--- Day-2_CRUD with JPA/src/com/cg/dao/JPAUtil.java
+++ Day-2_CRUD with JPA/src/com/cg/dao/JPAUtil.java
@@ -1,5 +1,12 @@
 package com.cg.dao;
 
+import javax.persistence.EntityManager;
+
 public class JPAUtil {
+
+	public static EntityManager getEntityManager() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
